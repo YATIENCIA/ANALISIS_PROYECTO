f05679c7a0dfde273bd74a4b0e4fa8e1ba960841
YATIENCIA <33614080+YATIENCIA@users.noreply.github.com>
2018-11-26 20:20:24 -0500

    Añadir archivos base

---
+++ PROYECTO1P_AA/InsertionSort.py
@@ -0,0 +1,42 @@
+#Aqui va el código del InsertionSort
+
+#Metodo1
+def insertionsort(A):
+    #we start loop at second element (index 1) since the first item is already sorted
+    for j in range(1,len(A)):
+        key = A[j] #The next item we are going to insert into the sorted section of the array
+
+        i = j-1 #the last item we are going to compare to
+        #now we keep moving the key back as long as it is smaller than the last item in the array
+        while (i > -1) and key < A[i]: #if i == -1 means that this key belongs at the start
+            A[i+1]=A[i] #move the last object compared one step ahead to make room for key
+            i=i-1 #observe the next item for next time.
+        #okay i is not greater than key means key belongs at i+1
+        A[i+1] = key
+    return A
+
+#Metodo2
+def insertionSort(alist):
+   for index in range(1,len(alist)):
+
+     currentvalue = alist[index]
+     position = index
+
+     while position>0 and alist[position-1]>currentvalue:
+         alist[position]=alist[position-1]
+         position = position-1
+
+     alist[position]=currentvalue
+
+
+
+
+#Pruebas
+alist = [54,26,93,17,77,31,44,55,20]
+insertionSort(alist)
+print(alist)
+
+if __name__=="__main__":
+    x = [5,2,4,6,1,3]
+    insertionsort(x)
+    print (x)
